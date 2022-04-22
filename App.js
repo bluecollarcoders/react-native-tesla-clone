73ff4907f92386cc0ab151ebdf30443181192924
Caleb Matteis <matteismusic@gmail.com>
2022-04-22 06:22:58 -0400

    commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        {/* card container */}
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Starting at 69,000</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  subTitle: {
+    fontSize: 20,
+    color: "#5c5e62",
   },
 });
